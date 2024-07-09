9cde3ec0d8c3cd108a2413a6a086cf4a84573dc6
Zeeeen0 <hoo1042@gmail.com>
2024-07-09 22:49:23 +0900

    [GGB-01] GraduationGoodbyeApplication 오타 수정

--- src/main/java/com/ggb/graduationgoodbye/GraduationGoodbyeApplication.java
+++ src/main/java/com/ggb/graduationgoodbye/GraduationGoodbyeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraduationgoodbyeApplication {
+public class GraduationGoodbyeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraduationgoodbyeApplication.class, args);
+		SpringApplication.run(GraduationGoodbyeApplication.class, args);
 	}
 
 }
