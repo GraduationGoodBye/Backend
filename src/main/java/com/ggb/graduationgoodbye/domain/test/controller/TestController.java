2cc66894d141ae00fc3248a64139240b90401539
hyensukim <crazymin17@naver.com>
2024-07-13 02:49:05 +0900

    [GGB-07] S3 연동 및 테스트

--- src/main/java/com/ggb/graduationgoodbye/domain/test/controller/TestController.java
+++ src/main/java/com/ggb/graduationgoodbye/domain/test/controller/TestController.java
@@ -2,18 +2,25 @@ package com.ggb.graduationgoodbye.domain.test.controller;
 
 import com.ggb.graduationgoodbye.domain.auth.exception.ExpiredTokenException;
 import com.ggb.graduationgoodbye.domain.auth.exception.InvalidTokenException;
+import com.ggb.graduationgoodbye.domain.test.service.TestService;
 import com.ggb.graduationgoodbye.global.error.exception.BusinessException;
 import com.ggb.graduationgoodbye.global.error.exception.ForbiddenException;
 import com.ggb.graduationgoodbye.global.error.exception.UnauthenticatedException;
 import com.ggb.graduationgoodbye.global.response.ApiResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    private final TestService testService;
+
+    public TestController(TestService testService){
+        this.testService = testService;
+    }
 
     @GetMapping("/check")
     public ApiResponse<String> check(){
@@ -31,5 +38,11 @@ public class TestController {
             case "INTERNAL_SERVER_ERROR": throw new RuntimeException("INTERNAL_SERVER_ERROR");
             default: throw new RuntimeException("default");
         }
+    }
+
+    @PostMapping("/image")
+    public ApiResponse<String> image(@RequestPart(value = "file") MultipartFile file) throws IOException {
+        String url = testService.uploadImageTest(file);
+        return ApiResponse.ok(url);
     }
 }
--- src/main/java/com/ggb/graduationgoodbye/domain/test/service/TestService.java
+++ src/main/java/com/ggb/graduationgoodbye/domain/test/service/TestService.java
@@ -1,4 +1,36 @@
 package com.ggb.graduationgoodbye.domain.test.service;
 
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class TestService {
+    private final S3Template s3Template;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String uploadImageTest(MultipartFile image) throws IOException {
+        String originalFilename = image.getOriginalFilename(); // 클라이언트가 전송한 파일 이름
+        String extension = StringUtils.getFilenameExtension(originalFilename); // 파일 확장자
+        String uuidImageName = UUID.randomUUID() + "." + extension; // 파일 이름 중복 방지
+        // S3에 파일 업로드
+        S3Resource s3Resource = s3Template.upload(bucketName,uuidImageName,image.getInputStream(),
+                ObjectMetadata.builder().contentType(image.getContentType()).build());
+        // 업로드 된 이미지 url 반환
+        return s3Resource.getURL().toString();
+    }
 }
