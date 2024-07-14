8e5f96e8c0fbb8553f17c1d7c7592b1e367c9f7e
Zeeeen0 <hoo1042@gmail.com>
2024-07-14 11:54:11 +0900

    [GGB-08] TestController.exception 파라미터 swagger 추가

--- src/main/java/com/ggb/graduationgoodbye/domain/test/controller/TestController.java
+++ src/main/java/com/ggb/graduationgoodbye/domain/test/controller/TestController.java
@@ -7,7 +7,9 @@ import com.ggb.graduationgoodbye.global.error.exception.ForbiddenException;
 import com.ggb.graduationgoodbye.global.error.exception.UnauthenticatedException;
 import com.ggb.graduationgoodbye.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,17 @@ public class TestController {
 
     @Operation(summary = "연결 확인")
     @GetMapping("/check")
-    public ApiResponse<String> check(){
+    public ApiResponse<?> check(){
         return ApiResponse.ok("This service is available");
     }
 
     @GetMapping("/exception/{name}")
-    public void exception(@PathVariable String name){
+    public void exception(
+            @Parameter(
+                    in = ParameterIn.PATH,
+                    schema = @Schema(type = "string", allowableValues = {"UNAUTHENTICATED", "FORBIDDEN", "BAD_REQUEST", "EXPIRED_TOKEN", "INVALID_TOKEN", "INTERNAL_SERVER_ERROR"})
+            )
+            @PathVariable String name){
         switch (name) {
             case "UNAUTHENTICATED": throw new UnauthenticatedException();
             case "FORBIDDEN": throw new ForbiddenException();
