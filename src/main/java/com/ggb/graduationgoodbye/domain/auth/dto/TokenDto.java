70ae978f713ad363197c7263f4310a3165ff9645
hyensukim <crazymin17@naver.com>
2024-08-09 15:04:01 +0900

    [GGB-104] TokenDto record로 관리

--- src/main/java/com/ggb/graduationgoodbye/domain/auth/dto/TokenDto.java
+++ src/main/java/com/ggb/graduationgoodbye/domain/auth/dto/TokenDto.java
@@ -1,18 +1,11 @@
 package com.ggb.graduationgoodbye.domain.auth.dto;
 
-import com.ggb.graduationgoodbye.domain.member.controller.TokenResponse;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class TokenDto {
-
-  private String accessToken;
-  private String refreshToken;
+public record TokenDto(
+    String accessToken,
+    String refreshToken
+) {
 
 }
