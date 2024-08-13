cffb424f751cd8d9a34dfbfb29e2ccca443ba231
hyensukim <crazymin17@naver.com>
2024-08-13 21:40:31 +0900

    [GGB-107] Member 필드 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,23 @@
 CREATE TABLE members
 (
     id       BIGINT AUTO_INCREMENT,
+    sns_type VARCHAR(20),
+    sns_id   VARCHAR(255),
     email    VARCHAR(100),
     profile  VARCHAR(255),
-    name     VARCHAR(50),
     nickname VARCHAR(50),
     address  VARCHAR(255),
-    phone    VARCHAR(255),
     gender   VARCHAR(10),
+    age      TINYINT,
+    phone    VARCHAR(255),
     role     VARCHAR(20),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE tokens
 (
-    id          BIGINT AUTO_INCREMENT,
-    user_id     VARCHAR(100) UNIQUE,
+    id            BIGINT AUTO_INCREMENT,
+    user_id       VARCHAR(100) UNIQUE,
     refresh_token VARCHAR(255),
     PRIMARY KEY (id)
 );
