476a498f7f958a04529ff27af92c5651071c72ee
hyensukim <crazymin17@naver.com>
2024-08-13 21:52:47 +0900

    [GGB-107] OpenFeign 의존성 추가 및 활성화

--- src/main/java/com/ggb/graduationgoodbye/GraduationGoodByeApplication.java
+++ src/main/java/com/ggb/graduationgoodbye/GraduationGoodByeApplication.java
@@ -2,8 +2,10 @@ package com.ggb.graduationgoodbye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GraduationGoodByeApplication {
 
   public static void main(String[] args) {
