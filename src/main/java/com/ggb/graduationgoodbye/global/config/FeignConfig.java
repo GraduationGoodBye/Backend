a8b5cc5b0cb54c3aa84fe6d8c998852e84937a9d
hyensukim <crazymin17@naver.com>
2024-08-13 22:14:15 +0900

    [GGB-107] FeignConfig 추가

---
+++ src/main/java/com/ggb/graduationgoodbye/global/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.ggb.graduationgoodbye.global.config;
+
+import feign.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+  @Bean
+  public Client feignClient() {
+    return new Client.Default(null, null);
+  }
+}
