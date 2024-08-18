523dc4c5d5e1e7cd16849fdc6a7cec21f6eebae8
JinTaekLim <jintaek019@gmail.com>
2024-08-18 15:45:11 +0900

    [GGB-117] schema.sql common_code 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS common_code;
 DROP TABLE IF EXISTS tokens;
 DROP TABLE IF EXISTS members;
 CREATE TABLE members
@@ -21,5 +22,17 @@ CREATE TABLE tokens
     id            BIGINT AUTO_INCREMENT,
     user_id       VARCHAR(100) UNIQUE,
     refresh_token VARCHAR(255),
+    PRIMARY KEY (id)
+);
+CREATE TABLE common_code
+(
+    id	        BIGINT	        NOT NULL AUTO_INCREMENT COMMENT '공통코드_ID',
+    type        varchar(255)    NOT NULL                COMMENT '대학,학과,사진타입,작품카테고리',
+    code        varchar(255)    NOT NULL                COMMENT '코드',
+    name        varchar(255)    NOT NULL                COMMENT '이름',
+    created_id  varchar(255)    NOT NULL                COMMENT '생성자',
+    created_at  datetime        NOT NULL                COMMENT '생성일시',
+    updated_id  varchar(255)	NULL                    COMMENT '수정자(null)',
+    updated_at  datetime	    NULL                    COMMENT '수정일시(null)',
     PRIMARY KEY (id)
 );
