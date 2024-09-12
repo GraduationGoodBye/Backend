5d3b9ccf56cfde6606d0827dbbd952d33202b91a
hyensukim <crazymin17@naver.com>
2024-09-12 17:08:31 +0900

    [GGB-126] 문자열 마스킹 처리 관련 테스트 케이스 분리

--- src/test/java/com/ggb/graduationgoodbye/domain/utils/StringConverterTest.java
+++ src/test/java/com/ggb/graduationgoodbye/domain/utils/StringConverterTest.java
@@ -5,34 +5,69 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.ggb.graduationgoodbye.global.test.ServiceTest;
 import com.ggb.graduationgoodbye.global.utils.StringConverter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringConverterTest extends ServiceTest {
 
   @Test
-  void convertToMasking_success() {
+  @DisplayName("convertToMasking_문자 패턴_성공")
+  void convertToMasking_character_success() {
     String input = "MEMBER";
     Character maskChar = '*';
-    String maskStr1 = "@!";
-    String maskStr2 = "null";
 
     String maskedByChar = StringConverter.convertToMasking(input, maskChar);
-    String maskedByStr1 = StringConverter.convertToMasking(input, maskStr1);
-    String maskedByStr2 = StringConverter.convertToMasking(input, maskStr2);
 
     assertEquals(maskedByChar, "******");
-    assertEquals(maskedByStr1, "@!@!@!");
-    assertEquals(maskedByStr2, "nullnu");
   }
 
   @Test
-  void convertToMasking_fail() {
+  @DisplayName("convertToMasking_문자 패턴_실패_null")
+  void convertToMasking_character_fail_null() {
     String input = "MEMBER";
     Character maskChar = null;
-    String maskStr = null;
 
     assertThrows(IllegalArgumentException.class,
         () -> StringConverter.convertToMasking(input, maskChar));
+  }
+
+  @Test
+  @DisplayName("convertToMasking_문자열 패턴_성공")
+  void convertToMasking_String_success() {
+    String input = "Member";
+    String maskStr = "@!";
+
+    String maskedByStr = StringConverter.convertToMasking(input, maskStr);
+
+    assertEquals(maskedByStr, "@!@!@!");
+  }
+
+  @Test
+  @DisplayName("convertToMasking_문자열 패턴_실패_null")
+  void convertToMasking_String_fail_null() {
+    String input = "Member";
+    String maskStr = null;
+
+    assertThrows(IllegalArgumentException.class,
+        () -> StringConverter.convertToMasking(input, maskStr));
+  }
+
+  @Test
+  @DisplayName("convertToMasking_문자열 패턴_실패_빈값")
+  void convertToMasking_String_fail_blank() {
+    String input = "Member";
+    String maskStr = "";
+
+    assertThrows(IllegalArgumentException.class,
+        () -> StringConverter.convertToMasking(input, maskStr));
+  }
+
+  @Test
+  @DisplayName("convertToMasking_문자열 패턴_실패_공백")
+  void convertToMasking_String_fail_space() {
+    String input = "Member";
+    String maskStr = " ";
+
     assertThrows(IllegalArgumentException.class,
         () -> StringConverter.convertToMasking(input, maskStr));
   }
