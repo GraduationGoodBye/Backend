40a26657dc644b7fdd796de458da135517efe321
hyensukim <crazymin17@naver.com>
2024-09-23 14:35:38 +0900

    [GGB-116] WithdrawProcessor 테스트 코드 작성

---
+++ src/test/java/com/ggb/graduationgoodbye/domain/member/business/WithdrawProcessorTest.java
@@ -0,0 +1,36 @@
+package com.ggb.graduationgoodbye.domain.member.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import com.ggb.graduationgoodbye.domain.member.common.entity.Member;
+import com.ggb.graduationgoodbye.domain.member.common.enums.SnsType;
+import com.ggb.graduationgoodbye.global.test.ServiceTest;
+import org.junit.jupiter.api.Test;
+
+public class WithdrawProcessorTest extends ServiceTest {
+
+  private final WithdrawProcessor withdrawProcessor = new WithdrawProcessor();
+
+  @Test
+  void processToWithdraw_성공() {
+    SnsType snsType = SnsType.GOOGLE;
+    String snsId = "testSnsId";
+    String mockData = "12345";
+    Member mockMember = Member.builder()
+        .snsType(snsType)
+        .snsId(snsId)
+        .email(mockData)
+        .profile(mockData)
+        .nickname(mockData)
+        .build();
+
+    withdrawProcessor.processToWithdraw(mockMember);
+
+    assertEquals(snsType, mockMember.getSnsType());
+    assertEquals(snsId, mockMember.getSnsId());
+    assertNotEquals(mockData, mockMember.getEmail());
+    assertNotEquals(mockData, mockMember.getProfile());
+    assertNotEquals(mockData, mockMember.getNickname());
+  }
+}
